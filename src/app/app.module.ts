ff4613dccdf6018f1f1e003925b77b9eea95451b
Unknown <suti211@gmail.com>
2017-11-26 01:24:48 +0100

    Project specific stuff

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import { GraphComponent } from './components/graph/graph.component';
+import { MockService} from './services/impl/MockService';
+import { ChartModule } from 'angular-highcharts';
+import { FormsModule } from '@angular/forms';
+import { FiltermodalComponent } from './components/filtermodal/filtermodal.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GraphComponent,
+    FiltermodalComponent
   ],
   imports: [
-    BrowserModule
+    FormsModule,
+    BrowserModule,
+    ChartModule,
   ],
-  providers: [],
+  providers: [MockService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
